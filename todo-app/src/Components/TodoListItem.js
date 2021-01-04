c7b2757e6ab7ceb7f53fea9fb830bfe792918216
jungeun-p <mrk7753@naver.com>
2021-01-04 18:56:17 +0900

    [Finish] TodoList CRUD complete

--- todo-app/src/Components/TodoListItem.js
+++ todo-app/src/Components/TodoListItem.js
@@ -3,16 +3,16 @@ import cn from 'classnames';
 import './TodoListItem.scss';
 import '@fortawesome/fontawesome-free/js/all.js';
 
-const TodoListItem = ({todo}) => {
+const TodoListItem = ({todo, onRemove, onToggle}) => {
     //todo 데이터는 통째로 props로 전달
-    const {text, checked} = todo;
+    const { id, text, checked} = todo;
     return (
         <div class="TodoListItem">
-            <div class={cn('checkbox', {checked})}>
+            <div class={cn('checkbox', {checked})} onClick={()=>{onToggle(id)}}>
                 {checked? <i className="far fa-check-square"></i> : <i className="far fa-square"></i>}
               <div class="text">{text}</div>
             </div>
-            <div class="remove">
+            <div class="remove" onClick={()=>onRemove(id)}>
               <i class="fas fa-eraser"></i>
             </div> 
         </div>

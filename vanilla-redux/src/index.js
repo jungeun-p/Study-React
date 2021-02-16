18f89edb5006d73a2744529357d549207e9880e7
jungeun Park <mrk7753@naver.com>
2021-02-17 07:24:47 +0900

    [ Delete ] Redux-study folder

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+const add = document.getElementById("add");
+const number = document.getElementById("span");
+const minus = document.getElementById("minus");
+
+// reducer 함수 (빈)
+// 데이터를 저장하고 변경하는 곳
+const countModifier = () => {};
+
+// 스토어
+const countStore = createStore(countModifier);
+console.log(countStore);

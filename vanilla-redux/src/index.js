9fcbeabd6e439abe4d8260770ce38e91371e20dc
jungeun-p <mrk7753@naver.com>
2021-02-20 22:28:17 +0900

    [ modify ] vanilla redux final

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,41 +1,79 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const number = document.getElementById("span");
-const minus = document.getElementById("minus");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-// reducer 함수 (빈)
-// 데이터를 변경하는 함수 (incerase, decrease)
-// return하는 것은 application에 있는 모든 data
-// reducer만 data를 modify
-// data는 reducer에만 존재한다.
-// action을 통해 호출된다
-const countModifier = (count = 1, action) => {
-  console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, id: Date.now() }];
+    case DELETE_TODO:
+      return state.filter((toDo) => toDo.id !== action.id);
+    default:
+      return state;
   }
 };
 
-// 스토어
-// 데이터를 저장하는 곳
-// 스토어를 만들면 reducer를 만들어 달라고 한다.
-const countStore = createStore(countModifier);
-console.log(countStore.getState());
-//getState(); state값을 얻는다
+const store = createStore(reducer);
 
-// 스토어에 countModifier -> initial state로 불러온다 -> state=0, initialState는 0이 된다
+// const addToDo = (text) => {
+//   store.dispatch({ type: ADD_TODO, text });
+// };
 
-// action
-// redux에서 fucntion을 부를 때 두번째 parameter or argument
-// countModifier(currentState=0, {type:'hello'}) 리듀스에게 메시지를 보내는 방법
-// dispatch와 메시지로 reducer에게 메시지 전달
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-console.log(countStore.getState());
+const dispatchAddToDo = (text) => {
+  //addToDo의 action을 담은 함수를 반환시킨다.
+  store.dispatch(addToDo(text));
+};
+
+// const deleteToDo = (e) => {
+//   const id = e.target.parentNode.id;
+//   store.dispatch({ type: DELETE_TODO, id });
+// };
+
+const dispatchDeleteTodo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteTodo);
+    li.id = toDo.id;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddToDo(toDo);
+};
+
+form.addEventListener("submit", onSubmit);

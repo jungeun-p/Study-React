d1c513d3ae898e54a128bacd72dbc3fca3ce9c1c
jungeun-p <mrk7753@naver.com>
2021-03-28 19:07:07 +0900

    [ modify ] createContext Color, Provider, Consumer

---
+++ context-tutorial/src/Context/color.js
@@ -0,0 +1,30 @@
+import { createContext } from "react";
+
+const ColorContext = createContext({
+  state: { color: "black", subcolor: "red" },
+  actions: {
+    setColor: () => {},
+    setSubcolor: () => {},
+  },
+});
+
+const ColorProvider = ({ children }) => {
+  const [color, setColor] = useState("black");
+  const [subcolor, setSubcolor] = useState("red");
+
+  const value = {
+    state: { color, subcolor },
+    actions: { setColor, setSubcolor },
+  };
+  return (
+    <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+  );
+};
+
+// const ColorConsumer = ColorContext.Consumer
+const { Consumer: ColorConsumer } = ColorContext;
+
+// ColorProvider ColorConsumer 내보내기
+const { ColorProvider, ColorConsumer };
+
+export default ColorContext;

b2012354faa63f84e33db308a2d0fc8dfb5e4cda
jungeun-p <mrk7753@naver.com>
2021-04-04 23:35:28 +0900

    [ modify ] redux-toolkit 업데이트
    -createAction를 통한 playload 업데이트

--- redux-tutorial/src/modules/todos.js
+++ redux-tutorial/src/modules/todos.js
@@ -1,13 +1,15 @@
-import { createAction, handleActions } from "redux-actions";
+import { handleActions } from "redux-actions";
 import produce from "immer";
+import { createAction } from "@reduxjs/toolkit";
 
-const CHANGE_INPUT = "todos/CHANGE_INPUT";
-const INSERT = "todos/INSERT";
-const TOGGLE = "todos/TOGGLE";
-const REMOVE = "todos/REMOVE";
+// const CHANGE_INPUT = "todos/CHANGE_INPUT";
+// const INSERT = "todos/INSERT";
+// const TOGGLE = "todos/TOGGLE";
+// const REMOVE = "todos/REMOVE";
 
 // export const changeInput = (input) => ({ type: CHANGE_INPUT, input });
-export const changeInput = createAction(CHANGE_INPUT, (input) => input);
+// export const changeInput = createAction(CHANGE_INPUT, (input) => input);
+export const changeInput = createAction("CHANGE_INPUT");
 
 let id = 3;
 // 호출시 id는 1씩 더해진다.
@@ -19,18 +21,20 @@ let id = 3;
 //     done: false,
 //   },
 // });
-
-export const insert = createAction(INSERT, (text) => ({
-  id: id++,
-  text,
-  done: false,
-}));
+// export const insert = createAction(INSERT, (text) => ({
+//   id: id++,
+//   text,
+//   done: false,
+// }));
+export const insert = createAction("INSERT");
 
 // export const toggle = (id) => ({ type: TOGGLE, id });
-export const toggle = createAction(TOGGLE, (id) => id);
+// export const toggle = createAction(TOGGLE, (id) => id);
+export const toggle = createAction("TOGGLE");
 
 // export const remove = (id) => ({ type: REMOVE, id });
-export const remove = createAction(REMOVE, (id) => id);
+// export const remove = createAction(REMOVE, (id) => id);
+export const remove = createAction("REMOVE");
 
 const initialState = {
   input: "",
@@ -39,69 +43,79 @@ const initialState = {
     { id: 2, text: "react with redux", done: false },
   ],
 };
+console.log(insert(), changeInput());
 
-// function todos(state = initialState, action) {
-//   switch (action.type) {
-//     case CHANGE_INPUT:
-//       return {
-//         ...state,
-//         input: action.input,
-//       };
-//     case INSERT:
-//       return {
-//         ...state,
-//         todos: state.todos.concat(action.todo),
-//       };
-//     case TOGGLE:
-//       return {
-//         ...state,
-//         todos: state.todos.map((todo) =>
-//           todo.id === action.id ? { ...todo, done: !todo.done } : todo
-//         ),
-//       };
-//     case REMOVE:
-//       return {
-//         ...state,
-//         todos: state.todos.filter((todo) => todo.id !== action.id),
-//       };
-//     default:
-//       return state;
-//   }
-// }
+function todos(state = initialState, action) {
+  switch (action.type) {
+    case changeInput.type:
+      console.log(action);
+      return {
+        ...state,
+        input: action.payload,
+      };
+    case insert.type:
+      // return {
+      //   ...state,
+      //   todos: state.todos.concat(action.payload),
+      // };
+      return {
+        ...state,
+        todos: state.todos.concat({
+          id: id++,
+          text: action.payload,
+          done: false,
+        }),
+      };
+    case toggle.type:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+        ),
+      };
+    case remove.type:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+}
 
-const todos = handleActions(
-  {
-    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
-    [INSERT]: (state, { payload: todo }) =>
-      // ({
-      //   ...state,
-      //   todos: state.todos.concat(todo),
-      // }),
-      produce(state, (draft) => {
-        draft.todos.push(todo);
-      }),
-    [TOGGLE]: (state, { payload: id }) =>
-      // ({
-      //   ...state,
-      //   todos: state.todos.map((todo) =>
-      //     todo.id === id ? { ...todo, done: !todo.done } : todo
-      //   ),
-      // }),
-      produce(state, (draft) => {
-        const todo = draft.todos.find((todo) => todo.id === id);
-        todo.done = !todo.done;
-      }),
-    [REMOVE]: (state, { payload: id }) =>
-      // ({
-      //   ...state,
-      //   todos: state.todos.filter((todo) => todo.id !== id),
-      // }),
-      produce(state, (draft) => {
-        const index = draft.todos.findIndex((todo) => todo.id === id);
-        draft.todos.splice(index, 1);
-      }),
-  },
-  initialState
-);
+// const todos = handleActions(
+//   {
+//     [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+//     [INSERT]: (state, { payload: todo }) =>
+//       // ({
+//       //   ...state,
+//       //   todos: state.todos.concat(todo),
+//       // }),
+//       produce(state, (draft) => {
+//         draft.todos.push(todo);
+//       }),
+//     [TOGGLE]: (state, { payload: id }) =>
+//       // ({
+//       //   ...state,
+//       //   todos: state.todos.map((todo) =>
+//       //     todo.id === id ? { ...todo, done: !todo.done } : todo
+//       //   ),
+//       // }),
+//       produce(state, (draft) => {
+//         const todo = draft.todos.find((todo) => todo.id === id);
+//         todo.done = !todo.done;
+//       }),
+//     [REMOVE]: (state, { payload: id }) =>
+//       // ({
+//       //   ...state,
+//       //   todos: state.todos.filter((todo) => todo.id !== id),
+//       // }),
+//       produce(state, (draft) => {
+//         const index = draft.todos.findIndex((todo) => todo.id === id);
+//         draft.todos.splice(index, 1);
+//       }),
+//   },
+//   initialState
+// );
 
 export default todos;
